3d9bdaf4060c6c8847d8423e577081272e2f607d
mahsa shadi <mahsa.shadi@mail.um.ac.ir>
2020-10-21 15:10:10 +0330

    first version of SingleStat widget is added

--- src/widgetItems.js
+++ src/widgetItems.js
@@ -1,9 +1,13 @@
 import StatsdWidget from './items/statsd';
 import GaugeWidget from './items/gauge';
 import BarWidget from './items/bars';
+import SingleStatWidget from './items/singlestat';
+//import BarChartAppexWidget from './items/barChart-apex';
+
 import statsdImage from '../XY-Chart.png';
 import gaugeImage from '../gauge-Chart.png';
 import barChartImage from '../bar-Chart.png';
+import singleStatImage from '../singleStat-Chart.png';
 
 const widgetItem = {
   statsd:{
@@ -20,6 +24,16 @@ const widgetItem = {
     object: BarWidget,
     name: 'BARCHART',
     image: barChartImage
-  }
+  },
+  singleStat:{
+    object: SingleStatWidget,
+    name: 'SINGLESTAT',
+    image: singleStatImage
+  },
+  // barChartAppex:{
+  //   object: BarChartAppexWidget,
+  //   name: 'SINGLESTAT',
+  //   image: barChartImage
+  // },
 };
 export default widgetItem;
